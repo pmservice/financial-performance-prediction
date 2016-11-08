d4aa22e027157171138a1d9a5e346d663039bfb2
LukaszCmielowski <Lukasz.Cmielowski@pl.ibm.com>
2016-11-08 10:19:48 +0100

    update

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -33,18 +33,18 @@ class Overview extends React.Component {
         <hr />
         <p>Time Series sample application gives you ability to work with financial and economic time series data to gain powerful insights into the future.</p>
         <p>Application delivers analytics-driven environment where you can explore time series from various perspectives and forecast future by using most suitable forecasting methods.</p>
-        <p>Application deployed on Predictive Analytics Service consists of two parts.</p>
+        <p>Application deployed on Watson Machine Learning Service consists of two parts.</p>
         <ol>
           <li>First part is about downloading financial and economical time series from open data sources and exploring them to see general characteristics such as trend, seasonality, return distributions and correlation between time series.</li>
           <li>Second part helps us to forecast near-future based on historical data with a level of confidence so that we can use time series analysis and forecast to solve our specific business problem.</li>
         </ol>
         <p>In practice, time series modeling is ambitious undertaking and it is used in many different fields to produce forecasts for different types of time series data, especially used today for many economic variables such as weekly sales figures, monthly returns of a stocks, inflation or gross domestic product.</p>
-        <p>Time series sample application is using IBM Predictive Analytics and IBM SPSS Modeler together to provide functionalities mentioned above.</p>
+        <p>Time series sample application is using IBM Watson Machine Learning and IBM SPSS Modeler together to provide functionalities mentioned above.</p>
         <p>Time Series Modeling node in IBM SPSS Modeler provides us methods such as exponential smoothing, univariate Autoregressive Integrated Moving Average (ARIMA), and multivariate ARIMA (or transfer function) models for time series.</p>
         <p>One of the greatest features of Time Series Modeling node is Expert Modeler. Expert Modeler, which automatically identifies and estimates the best-fitting ARIMA or exponential smoothing model for one or more target variables, thus eliminating the need to identify an appropriate model through trial and error.</p>
-        <p>IBM Predictive Analytics takes things even further and allows us to deploy streams that we develop in IBM SPSS Modeler to cloud environment to turn our data models into fully fledged business applications.</p>
-        <p>In the context of this application, IBM Predictive Analytics Service provides useful framework to gather, explore and forecast financial and economical time series in iterative nature without requiring repetitive manual steps and automating the whole end-to-end process.</p>
-        <p>One can see how powerful it can be to use IBM SPSS Modeler and IBM Predictive Analytics Service together to work with financial and economical time series and how IBM Predictive Analytics puts all these capabilities into the hands of its users.</p>
+        <p>IBM Watson Machine Learning takes things even further and allows us to deploy streams that we develop in IBM SPSS Modeler to cloud environment to turn our data models into fully fledged business applications.</p>
+        <p>In the context of this application, IBM Watson Machine Learning Service provides useful framework to gather, explore and forecast financial and economical time series in iterative nature without requiring repetitive manual steps and automating the whole end-to-end process.</p>
+        <p>One can see how powerful it can be to use IBM SPSS Modeler and IBM Watson Machine Learning Service together to work with financial and economical time series and how IBM Watson Machine Learning puts all these capabilities into the hands of its users.</p>
 
         <hr />
         <h4>Guidelines regarding preparation of stream</h4>
